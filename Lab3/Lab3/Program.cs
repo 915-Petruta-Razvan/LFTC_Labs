f1cbffd8ad20c2fab7db092970b2037bd11d2678
915-Petruta-Razvan <razvan.petruta@stud.ubbcluj.ro>
2023-11-02 18:01:10 +0200

    added lab 3

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -5,10 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Scanner scanner = new Scanner();
-        // scanner.Scan("Resources/p1.txt");
-        // scanner.Scan("Resources/p2.txt");
-        // scanner.Scan("Resources/p3.txt");
-        // scanner.Scan("Resources/p1err.txt");
-        scanner.Scan("Resources/testing.txt");
+        scanner.Scan("Resources/p1.txt");
+        scanner.Scan("Resources/p2.txt");
+        scanner.Scan("Resources/p3.txt");
+        scanner.Scan("Resources/p1err.txt");
     }
 }
