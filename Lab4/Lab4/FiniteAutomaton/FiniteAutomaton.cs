ead27773445e3ffd86453622202831b7691651ae
915-Petruta-Razvan <razvan.petruta@stud.ubbcluj.ro>
2023-11-11 19:24:52 +0200

    added lab 4 work

---
+++ Lab4/Lab4/FiniteAutomaton/FiniteAutomaton.cs
@@ -0,0 +1,158 @@
+using System.Text.RegularExpressions;
+
+namespace Lab4;
+
+public class FiniteAutomaton
+{
+    public List<string> States { get; set; }
+    public List<string> Alphabet { get; set; }
+    public List<Transition> Transitions { get; set; }
+    public string InitialState { get; set; }
+    public List<string> FinalStates { get; set; }
+    public string PathToFile { get; set; }
+
+    public FiniteAutomaton(string pathToFile)
+    {
+        PathToFile = pathToFile;
+        States = new List<string>();
+        Alphabet = new List<string>();
+        Transitions = new List<Transition>();
+        InitialState = "";
+        FinalStates = new List<string>();
+
+        try
+        {
+            InitFromFile();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    public void PrintStates()
+    {
+        PrintListOfStrings("states", States);
+    }
+    
+    public void PrintAlphabet()
+    {
+        PrintListOfStrings("alphabet", Alphabet);
+    }
+    
+    public void PrintTransitions()
+    {
+        Console.Write("transitions = {");
+        for (int i = 0; i < Transitions.Count; i++)
+        {
+            if (i != Transitions.Count - 1)
+            {
+                Console.Write($"({Transitions[i].FromState}, {Transitions[i].ToState}, {Transitions[i].Symbol}); ");
+            }
+            else
+            {
+                Console.Write($"({Transitions[i].FromState}, {Transitions[i].ToState}, {Transitions[i].Symbol})");
+            }
+        }
+        Console.WriteLine("}");
+    }
+    
+    public void PrintInitialState()
+    {
+        Console.WriteLine($"initial_state = {InitialState}");
+    }
+    
+    public void PrintFinalStates()
+    {
+        PrintListOfStrings("final_states", FinalStates);
+    }
+
+    public bool IsWordValid(string word)
+    {
+        List<string> tokens = word.Select(c => c.ToString()).ToList();
+        string currentState = InitialState;
+        foreach (string token in tokens)
+        {
+            bool found = false;
+            foreach (Transition transition in Transitions)
+            {
+                if (transition.FromState == currentState && transition.Symbol == token)
+                {
+                    currentState = transition.ToState;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                return false;
+            }
+        }
+
+        return FinalStates.Contains(currentState);
+    }
+
+    private void PrintListOfStrings(string name, List<string> list)
+    {
+        Console.Write($"{name} = {{");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write(i == list.Count - 1 ? $"{list[i]}" : $"{list[i]},");
+        }
+        Console.WriteLine("}");
+    }
+
+    private void InitFromFile()
+    {
+        Regex regex = new Regex("^([a-z_]*=)");
+        foreach (string line in File.ReadLines(PathToFile))
+        {
+            Match match = regex.Match(line);
+            if (!match.Success)
+            {
+                throw new Exception($"Invalid line: {line}");
+            }
+
+            switch (match.Value)
+            {
+                case "states=":
+                    States = GetLineRightHandSide(line).Split(",").ToList();
+                    break;
+                
+                case "alphabet=":
+                    Alphabet = GetLineRightHandSide(line).Split(",").ToList();
+                    break;
+                
+                case "transitions=":
+                    List<string> transitionStrings = GetLineRightHandSide(line).Split("|").ToList();
+                    foreach (string transitionString in transitionStrings)
+                    {
+                        string transitionStringNoParanthesis =
+                            transitionString.Substring(1, transitionString.Length - 2);
+                        List<string> transitionTokens = transitionStringNoParanthesis.Split(",").ToList();
+                        Transitions.Add(new Transition(transitionTokens[0], transitionTokens[1], transitionTokens[2]));
+                    }
+                    break;
+                
+                case "initial_state=":
+                    InitialState = GetLineRightHandSide(line);
+                    break;
+                
+                case "final_states=":
+                    FinalStates = GetLineRightHandSide(line).Split(",").ToList();
+                    break;
+                
+                default:
+                    throw new Exception($"Unrecognized line: {line}");
+            }
+        }
+    }
+
+    private string GetLineRightHandSide(string line)
+    {
+        string rightHandSide = line.Substring(line.IndexOf("=") + 1);
+        
+        return rightHandSide.Substring(1, rightHandSide.Length - 2);
+    }
+}
---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu menu = new Menu();
+        menu.Run();
+    }
+}
